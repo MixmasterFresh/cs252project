0d84bbd45dc0099471c53424b5c5dc0410aea8ae
AngieYu <yu208@purdue.edu>
2016-12-04 22:47:01 -0500

    Add files via upload

---
+++ login/login.controller.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location'];
+    function LoginController($location) {
+        var vm = this;
+        
+        function login() {
+            vm.dataLoading = true;
+            function Login(vm.username, vm.password) {
+                $http.post($location, JSON.stringify( { user: vm.uservm, passward: vm.password }))
+                .then(function(response){
+                    if(response[1] === 'success' || 'good'){
+                        $location.path('/home');
+                    }
+                     else {
+                        window.alert("login Fail");
+                        vm.dataLoading = false;
+                    }
+                });     
+            };
+        };
+    }
+
+});
+
