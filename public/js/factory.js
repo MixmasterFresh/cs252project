fc7bb68e67c81b2ecab0e5cbc4f03c7df1b89c16
Yu <yu208@purdue.lcl>
2016-12-04 21:12:00 -0500

    http change

--- public/js/factory.js
+++ public/js/factory.js
@@ -6,7 +6,7 @@
       var _factory = {};
 
       _factory.fetchFile = function(file) {
-        return $http.get('/api/resource?resource=' + encodeURIComponent(file));
+        return $http.post(encodeURIComponent(file));
       };
 
       return _factory;
